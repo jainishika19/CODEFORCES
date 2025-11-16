546401c49f820fc68977122b6277378d4ec525a6
Ishika Jain <jainishika654@gmail.com>
2025-11-16 16:29:14 +0530

    Delete max_and_min.py

--- max_and_min.py
+++
@@ -1,6 +0,0 @@
-a,b,c=map(int,input().split())
-# print(a if a<b and a<c else b if b<c and b<a else c,a if a>b and a>c else b if b>c and b>a else c
-minimum=min(a,b,c)
-maximum=max(a,b,c)
-print(minimum,maximum)
-
