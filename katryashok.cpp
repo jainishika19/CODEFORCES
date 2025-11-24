843e4dc0359ca90317521f0ac740c2dd574ad410
Ishika Jain <jainishika654@gmail.com>
2025-11-24 20:54:48 +0530

    Add files via upload

--- katryashok.cpp
+++ katryashok.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main(){
     long long n,m,k;
     cin>>n>>m>>k;
-    long use_mouth;
+    long long use_mouth;
     use_mouth=min(n,min(m,k));
     //if we have to find min(n,m,k) direct by passing 
     //3 values then syntax of thus is--min({m,n,k})
     n-=use_mouth;
     m-=use_mouth;
     k-=use_mouth;
-    long use_no_mouth=min(n/2,k);
-    long d=use_mouth+use_no_mouth;
+    long long use_no_mouth=min(n/2,k);
+    long long d=use_mouth+use_no_mouth;
     cout<<d;
 
 
