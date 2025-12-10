6cae9f10aa6cbe59ea4f0c3c872618e709f024e6
Ishika Jain <jainishika654@gmail.com>
2025-12-10 22:36:37 +0530

    Add files via upload

---
+++ numbers_histogram.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char s;
+    int N,n;
+    cin>>s;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        cin>>n;
+    
+    for(int j=0;j<n;j++){
+        cout<<s;
+    }
+    cout<<endl;
+
+    }
+    
+    return 0;
+}
